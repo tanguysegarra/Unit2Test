72d9e3da8767135d921d229290232fc2fafe182e
Tanguy Segarra <tanguy.segarra@epita.fr>
2019-12-29 22:30:36 +0100

    utest-fm: add some functions to test with my unit-testing framework

---
+++ somecode.py
@@ -0,0 +1,26 @@
+'''
+Add two numbers
+'''
+
+def add(a, b):
+
+    return a + b
+
+
+'''
+Concatenate two lists
+'''
+
+def concat(L1, L2, L3):
+
+    return L1 + L2 + L3
+
+
+'''
+Get length of a list
+'''
+
+def length(L):
+
+    # We will make this one wrong to test failed tests
+    return len(L) - 1
