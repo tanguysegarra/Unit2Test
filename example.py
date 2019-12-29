b98ae8147bd7a4ca2c6bbb90d25f3c8f1927b0da
Tanguy Segarra <tanguy.segarra@epita.fr>
2019-12-29 22:32:20 +0100

    utfm: add some tests as example to show how to use this unit-testing framework

---
+++ example.py
@@ -0,0 +1,30 @@
+import utfm
+
+funs = utfm.load_module("somecode.py")
+
+# ADDITION TESTS
+
+c_addition = utfm.Category("ADDITION")
+
+c_addition.add_test(utfm.Test(funs.add, [1, 1], 2))
+c_addition.add_test(utfm.Test(funs.add, [3, 4], 7))
+c_addition.add_test(utfm.Test(funs.add, [0, 0], 0))
+
+c_addition.check()
+
+# CONCATENATION TESTS
+
+c_concatenation = utfm.Category("CONCATENATION")
+
+c_concatenation.add_test(utfm.Test(funs.concat, [[1, 2], [3, 4], [5, 6]], [1, 2, 3, 4, 5, 6]))
+c_concatenation.add_test(utfm.Test(funs.concat, [[0], [], [42]], [0, 42]))
+
+c_concatenation.check()
+
+# LENGTH TESTS
+
+c_length = utfm.Category("LENGTH")
+
+c_length.add_test(utfm.Test(funs.length, [[1, 2, 3]], 3))
+
+c_length.check()
