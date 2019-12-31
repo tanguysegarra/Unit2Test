7561e2d0667e5f193ee7bf083d81843da7efce76
Tanguy Segarra <tanguy.segarra@epita.fr>
2019-12-31 15:50:04 +0100

    utfm: add quiet/verbose options parsing

--- utfm.py
+++ utfm.py
@@ -1,3 +1,5 @@
+from optparse import OptionParser
+
 import importlib
 
 
@@ -9,14 +11,14 @@ class Suite:
     def add_category(self, category):
         self.categories.append(category)
 
-    def run(self):
+    def run(self, opt = None):
         nb_tests = 0
         passed = 0
 
         print("")
         for c in self.categories:
             nb_tests += len(c.tests)
-            passed += c.check()
+            passed += c.check(opt)
 
         perc = 0
         if nb_tests != 0:
@@ -39,13 +41,13 @@ class Category:
     def add_test(self, test):
         self.tests.append(test)
 
-    def check(self):
+    def check(self, opt = None):
         passed = 0
 
         print("~~~ " + self.title + " ~~~")
 
         for t in self.tests:
-            passed += t.check()
+            passed += t.check(opt)
 
         print("")
 
@@ -59,22 +61,30 @@ class Test:
         self.inp = inp
         self.out = out
 
-    def check(self):
+    def check(self, opt = None):
         fmt_inp = ", ".join(str(x) for x in self.inp)
 
         try:
             res = self.function(*self.inp)
 
-
             if res == self.out:
                 print("\033[0;32m[OK]\033[0m ", end='')
-                print(f"{self.function.__name__}({fmt_inp}) = {res}")
+                if opt.verbose:
+                    print(f"{self.function.__name__}({fmt_inp}) = {res}", end='')
+
+                print("")
                 return 1
 
             else:
                 print("\033[0;31m[KO]\033[0m ", end='')
-                print(f"{self.function.__name__}({fmt_inp}) = {res} | Expected: {self.out}")
+                if opt.verbose:
+                    print(f"{self.function.__name__}({fmt_inp}) = {res} | Expected: {self.out}", end='')
+                else:
+                    print(f"Received: {res} | Expected: {self.out}", end='')
+
+                print("")
                 return 0
+
 
         except Exception as e:
             print(e, "\033[0;31m[K0]\033[0m")
@@ -86,3 +96,22 @@ def load_module(path):
         path = path[:-3]
 
     return importlib.import_module(path)
+
+
+def parse_options():
+
+    usage = "Usage: ./%prog [options]"
+
+    parser = OptionParser(usage=usage)
+
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
+                      default=False,
+                      help="display complete outputs to stdout")
+
+    parser.add_option("-q", "--quiet", action="store_false", dest="verbose",
+                      help="do not print complete outputs to stdout")
+
+    (options, args) = parser.parse_args()
+
+    #return (options, args)
+    return options
