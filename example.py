4e9522fa940ed846951582f4b407671559074217
Tanguy Segarra <tanguy.segarra@epita.fr>
2019-12-31 15:14:12 +0100

    example: integrate suite to example code

--- example.py
+++ example.py
@@ -1,31 +1,28 @@
-import utfm
+from utfm import *
 
-funs = utfm.load_module("somecode.py")
+funs = load_module("somecode.py")
 
 # ADDITION TESTS
 
-c_addition = utfm.Category("ADDITION")
+c_addition = Category("ADDITION")
 
-c_addition.add_test(utfm.Test(funs.add, [1, 1], 2))
-c_addition.add_test(utfm.Test(funs.add, [3, 4], 7))
-
-c_addition.check()
-
-utfm.Test(funs.add, [3, 4], 5).check()
+c_addition.add_test(Test(funs.add, [1, 1], 2))
+c_addition.add_test(Test(funs.add, [3, 4], 7))
 
 # CONCATENATION TESTS
 
-c_concatenation = utfm.Category("CONCATENATION")
+c_concatenation = Category("CONCATENATION")
 
-c_concatenation.add_test(utfm.Test(funs.concat, [[1, 2], [3, 4], [5, 6]], [1, 2, 3, 4, 5, 6]))
-c_concatenation.add_test(utfm.Test(funs.concat, [[0], [], [42]], [0, 42]))
-
-c_concatenation.check()
+c_concatenation.add_test(Test(funs.concat, [[1, 2], [3, 4], [5, 6]], [1, 2, 3, 4, 5, 6]))
+c_concatenation.add_test(Test(funs.concat, [[0], [], [42]], [0, 42]))
 
 # LENGTH TESTS
 
-c_length = utfm.Category("LENGTH")
+c_length = Category("LENGTH")
 
-c_length.add_test(utfm.Test(funs.length, [[1, 2, 3]], 3))
+c_length.add_test(Test(funs.length, [[1, 2, 3]], 3))
 
-c_length.check()
+# TEST SUITE RUNNER
+
+suite = Suite([c_addition, c_concatenation, c_length])
+suite.run()
